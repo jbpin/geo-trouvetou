fd5b56ca246e02830d2a834f286f40d651170687
jbpin <jibet.pin@gmail.com>
2015-02-21 12:29:27 +0100

    refactor code

--- geotrouvetou.js
+++ geotrouvetou.js
@@ -25,128 +25,125 @@ function GeoPoint(latitude,longitude){
 	
 	this.latitude = latitude;
 	this.longitude = longitude;
-	this.data;
-
-	this.getDirection = function(point){
-		// 0 NW
-		// 1 NE
-		// 2 SW
-		// 3 SE
-		var latdif = 0
-		if(this.latitude > point.latitude){
-			//South
-			latdif = 2;
-		}
-		if(this.longitude > point.longitude){
-			// East
-			latdif += 1;
-		}
-		return latdif;
-	}
-
-	this.equals = function(point){
-		if(point.latitude == this.latitude && point.longitude == this.longitude)
-			return true;
-	}
-
-	this.closer = function(p1,p2){
-		var sqdist1 = Math.pow(p1.latitude - this.latitude,2) + Math.pow(p1.longitude - this.longitude,2)
-		var sqdist2 = Math.pow(p2.latitude - this.latitude,2) + Math.pow(p2.longitude - this.longitude,2)
-		if(sqdist1 < sqdist2){
-			return p1;
-		}else{
-			return p2;
-		}
-	}
+	this.data = null;
 }
+
+GeoPoint.prototype.getDirection = function(point) {
+	// 0 NW
+	// 1 NE
+	// 2 SW
+	// 3 SE
+	var latdif = 0;
+	if(this.latitude > point.latitude){
+		//South
+		latdif = 2;
+	}
+	if(this.longitude > point.longitude){
+		// East
+		latdif += 1;
+	}
+	return latdif;
+};
+
+GeoPoint.prototype.equals = function(point){
+	if(point.latitude == this.latitude && point.longitude == this.longitude)
+		return true;
+};
+
+GeoPoint.prototype.closer = function(p1,p2){
+	var sqdist1 = Math.pow(p1.latitude - this.latitude,2) + Math.pow(p1.longitude - this.longitude,2);
+	var sqdist2 = Math.pow(p2.latitude - this.latitude,2) + Math.pow(p2.longitude - this.longitude,2);
+	if(sqdist1 < sqdist2){
+		return p1;
+	}else{
+		return p2;
+	}
+};
 
 
 function TreeNode(point){
-
 	this.point = point;
 	this.leaf = [];
+}
 
-	this.addLeaf = function(point){
-		var direction = this.point.getDirection(point);
-		if(!this.leaf[direction]){
-			this.leaf[direction] = new TreeNode(point);
+TreeNode.prototype.addLeaf = function(point){
+	var direction = this.point.getDirection(point);
+	if(!this.leaf[direction]){
+		this.leaf[direction] = new TreeNode(point);
+	}else{
+		this.leaf[direction].addLeaf(point);
+	}
+};
+
+TreeNode.prototype.logPoint = function(point){
+	console.log(point.latitude+" "+point.longitude);
+};
+
+TreeNode.prototype.find = function(point){
+	var direction = this.point.getDirection(point);
+	if(!this.leaf[direction]){
+		return this;
+	}else{
+		var n = this.leaf[direction].find(point);
+		var p = point.closer(n.point,this.point);
+		if(p == this.point)
+			n = this;
+		var c,c1,c2;
+		if(direction === 0 || direction === 3){
+			if(this.leaf[1])
+				c1 = this.leaf[1].find(point);
+			if(this.leaf[2])
+				c2 = this.leaf[2].find(point);
 		}else{
-			this.leaf[direction].addLeaf(point);
+			if(this.leaf[0])
+				c1 = this.leaf[0].find(point);
+			if(this.leaf[3])
+				c2 = this.leaf[3].find(point);
 		}
-	}
-
-	this.logPoint = function(point){
-		console.log(point.latitude+" "+point.longitude);
-	}
-
-	this.find = function(point){
-		var direction = this.point.getDirection(point);
-		if(!this.leaf[direction]){
-			return this;
+		if(c1 && c2){
+			p = point.closer(c1.point,c2.point);
+			if(p == c1.point)
+				c = c1;
+			else
+				c = c2;
+		}else if(c1){
+			c = c1;
+		}else if(c2){
+			c = c2;
 		}else{
-			var n = this.leaf[direction].find(point);
-			var p = point.closer(n.point,this.point);
-			if(p == this.point)
-				n = this;
-			var c,c1,c2;
-			if(direction == 0 || direction == 3){
-				if(this.leaf[1])
-					c1 = this.leaf[1].find(point);
-				if(this.leaf[2])
-					c2 = this.leaf[2].find(point);
-			}else{
-				if(this.leaf[0])
-					c1 = this.leaf[0].find(point);
-				if(this.leaf[3])
-					c2 = this.leaf[3].find(point);
-			}
-			if(c1 && c2){
-				p = point.closer(c1.point,c2.point);
-				if(p == c1.point)
-					c = c1;
-				else
-					c = c2;
-			}else if(c1){
-				c = c1
-			}else if(c2){
-				c = c2	
-			}else{
-				return n;
-			}
-			p = point.closer(n.point, c.point);
-			if(p == c.point){
-				n = c;
-			}
 			return n;
 		}
+		p = point.closer(n.point, c.point);
+		if(p == c.point){
+			n = c;
+		}
+		return n;
 	}
-
-}
+};
 
 function GeoTrouvetou(){
-
 	this.tree = null;
-
-	this.addPoint = function(point){
-		if(!this.tree){
-			this.tree = new TreeNode(point)
-		}else{
-			this.tree.addLeaf(point)
-		}
-	}
-
-	this.findClosest = function(point){
-		if(this.tree == null)
-			return null;
-		return this.tree.find(point).point;
-	}
 }
+
+GeoTrouvetou.prototype.addPoint = function(point){
+	if(!this.tree){
+		this.tree = new TreeNode(point);
+	}else{
+		this.tree.addLeaf(point);
+	}
+};
+
+GeoTrouvetou.prototype.findClosest = function(point){
+	if(this.tree === null)
+		return null;
+	return this.tree.find(point).point;
+};
 
 //Node js export
 if (typeof module !== 'undefined' && module.exports) {
 	module.exports = {
 		GeoTrouvetou : GeoTrouvetou,
 		GeoPoint : GeoPoint
-	}
+	};
 }
 
